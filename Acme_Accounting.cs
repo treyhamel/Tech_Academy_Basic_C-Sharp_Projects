d023908b8553765ab80d8db037830c882b14ff1d
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-05 16:44:08 -0500

    Create Acme_Accounting.cs

---
+++ Acme_Accounting.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Account Systems");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.Read();
+    }
+}
+
