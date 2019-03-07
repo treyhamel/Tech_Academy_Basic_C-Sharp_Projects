88387bcf1604e6d128bbfa5c68edc8726fedf700
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-07 09:41:28 -0500

    Page 94 Assignment

---
+++ Page94_Task1/Page94_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page94_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the 9th prime number?");
+            double answer = Convert.ToDouble(Console.ReadLine());
+            while (answer != 23)
+            {
+                if (answer < 23)
+                {
+                    Console.WriteLine("Your guess is too small. Try again.");
+                    answer = Convert.ToDouble(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too large. Try again.");
+                    answer = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("That's correct! 23 is the 9th prime number.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Page94_Task2/Page94_Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page94_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name a factor of the number 1001");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (answer == 7)
+                {
+                    Console.WriteLine("That's correct! " + answer + " is a factor of 1001.");
+                    Console.ReadLine();
+                }
+                else if (answer == 11)
+                {
+                    Console.WriteLine("That's correct! " + answer + " is a factor of 1001.");
+                    Console.ReadLine();
+                }
+                else if (answer == 13)
+                {
+                    Console.WriteLine("That's correct! " + answer + " is a factor of 1001.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong. " + answer + " is not a factor of 1001. Try again");
+                    Console.WriteLine("Name a factor of the number 1001");
+                    answer = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            while (answer != 7 && answer != 11 && answer != 13);
+            Console.WriteLine("That's correct! " + answer + " is a factor of 1001.");
+            Console.ReadLine();
+        }
+    }
+}
