432a0abc050ef22a92d40baf86065663c9312022
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-08 12:10:45 -0500

    Add Page 111 Task

---
+++ Page111_Task/Page111_Task/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page111_Task
+{
+    class Operations
+    {
+        public static int Square(int numOne)
+        {
+            int result = numOne * numOne;
+            return result;
+        }
+        public static int Divide(int numOne)
+        {
+            int result = numOne / 2;
+            return result;
+        }
+        public static int Subtract(int numOne)
+        {
+            int result = numOne - 2;
+            return result;
+        }
+    }
+}
---
+++ Page111_Task/Page111_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page111_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            int divide = Operations.Divide(numOne);
+            int subtract = Operations.Subtract(numOne);
+            int square = Operations.Square(numOne);
+
+            Console.WriteLine(numOne + " squared is " + square);
+            Console.WriteLine(numOne + " divided by 2 is " + divide);
+            Console.WriteLine(numOne + " minus 2 is " + subtract);
+            Console.ReadLine();
+        }
+    }
+}
