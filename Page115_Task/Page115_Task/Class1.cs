6a84ebe97ecc7b63e1ea8bb04ee82d661954a3ef
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-11 09:40:22 -0400

    Add page 115 task

---
+++ Page115_Task/Page115_Task/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page115_Task
+{
+    class Class1
+    {
+        public int TwoInt(int int1, int int2 = 0)
+        {
+            int result = int1 * int1 + int2 * int2;
+            return result;
+        }
+    }
+}
---
+++ Page115_Task/Page115_Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page115_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 function = new Class1();
+            Console.WriteLine("Please input the first whole number.");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second whole number, or leave this blank.");
+            string input2 = Console.ReadLine();
+            if (input2 == "")
+            {
+                Console.WriteLine("The sum of the squares of the number(s) you input is: " + function.TwoInt(int1));
+            }
+            else
+            {
+                int int2 = Convert.ToInt32(input2);
+                Console.WriteLine("The sum of the square of the number(s) you input is: " + function.TwoInt(int1, int2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
