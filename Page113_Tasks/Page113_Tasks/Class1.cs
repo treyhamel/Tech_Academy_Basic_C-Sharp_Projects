2d9fc034db9d78a9a64e190018b6060a3d488a8a
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-11 09:08:27 -0400

    Add Page 113 Tasks

---
+++ Page113_Tasks/Page113_Tasks/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page113_Tasks
+{
+    class Class1
+    {
+        public void TwoInt(int int1, int int2)
+        {
+            int square = int1 * int1;
+            Console.WriteLine("The second number you put into this method is " + int2);
+        }
+    }
+}
---
+++ Page113_Tasks/Page113_Tasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page113_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 function = new Class1();
+            function.TwoInt(5, 10);
+
+            function.TwoInt(int1: 20, int2: 100);
+            Console.ReadLine();
+        }
+    }
+}
