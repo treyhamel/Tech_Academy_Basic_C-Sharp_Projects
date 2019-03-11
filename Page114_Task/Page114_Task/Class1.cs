fe605135dd5362bfc75bc41551c5986d0c1670aa
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-11 09:24:39 -0400

    Add Page 114 task

---
+++ Page114_Task/Page114_Task/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page114_Task
+{
+    class Class1
+    {
+        public int Operation(int int1)
+        {
+            int result = int1 * int1 * int1;
+            return result;
+        }
+
+        public int Operation(double dub1)
+        {
+            int result = Convert.ToInt32(dub1 * dub1 * dub1);
+            return result;
+        }
+
+        public int Operation(string str1)
+        {
+            int int1 = Convert.ToInt32(str1);
+            int result = int1 * int1 * int1;
+            return result;
+        }
+    }
+}
---
+++ Page114_Task/Page114_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page114_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 function = new Class1();
+            Console.WriteLine(function.Operation(5));
+
+            Console.WriteLine(function.Operation(6.0));
+
+            Console.WriteLine(function.Operation("10"));
+            Console.ReadLine();
+        }
+    }
+}
