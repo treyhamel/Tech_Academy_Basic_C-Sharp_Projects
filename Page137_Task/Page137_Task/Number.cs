07e296501fa7f896cc7a432bc5024337d5a42a4e
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-12 14:19:59 -0400

    Add Page 137 Task

---
+++ Page137_Task/Page137_Task/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page137_Task
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Page137_Task/Page137_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page137_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 256100.359m;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
