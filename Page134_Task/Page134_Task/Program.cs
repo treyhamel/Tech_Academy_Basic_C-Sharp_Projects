b5a48c8db53355e961451e75085af64b9b227c44
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-12 13:55:25 -0400

    Add page 134 task

---
+++ Page134_Task/Page134_Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page134_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week.");
+            try
+            {
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine("Today is " + day);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
