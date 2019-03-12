83b7537d30f9c46c0cb8ab1823afa02bc0968f4f
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-12 11:10:39 -0400

    Add Page 131 Task

---
+++ Page131_Task/Page131_Task/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page131_Task
+{
+    public class Employee<T>
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Page131_Task/Page131_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page131_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> empStr = new Employee<string>();
+            empStr.Things = new List<string>()
+                { "Longing", "Rusted", "Seventeen", "Daybreak", "Furnace", "Nine", "Benign", "Homecoming", "One", "Freight car"};
+
+            Employee<int> empInt = new Employee<int>();
+            empInt.Things = new List<int>()
+                { 87, 65, 78, 1, 53, 21, 28, 18};
+
+            foreach (string thing in empStr.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in empInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
