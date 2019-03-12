8cf357279b597bced7417718cb13f5bf139de4c4
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-12 10:36:28 -0400

    Add Page 129 Task

---
+++ Page129_Task/Page129_Task/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page129_Task
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return (employee1.Id == employee2.Id);
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return (employee1.Id != employee2.Id);
+        }
+    }
+}
---
+++ Page129_Task/Page129_Task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page129_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Id = 13;
+
+            Employee employee2 = new Employee();
+            employee2.Id = 22;
+
+            Employee employee3 = new Employee();
+            employee3.Id = 13;
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 == employee3);
+            Console.WriteLine(employee3 != employee2);
+
+            Console.ReadLine();
+        }
+    }
+}
