633e1bff9159948285dffaca3d2fdadc789f789f
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-13 12:14:09 -0400

    Add Page 154 task

---
+++ Page154_Task/Page154_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Page154_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number, your favorite number.");
+            string favNum = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Thamel\logs\log1.txt", favNum);
+            string response = File.ReadAllText(@"C:\Users\Thamel\logs\log1.txt");
+            Console.WriteLine("Your favorite number is {0}.", response);
+            Console.ReadLine();
+        }
+    }
+}
