db4fbab47b44a9449738857fc9ee1ff11b3aebe7
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-13 13:40:25 -0400

    Add Page 156 Task

---
+++ Page156_Task/Page156_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page156_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = new DateTime();
+            current = DateTime.Now;
+            Console.WriteLine(current);
+            Console.WriteLine("Please input a whole number");
+            int response = Convert.ToInt32(Console.ReadLine());
+
+            DateTime future = new DateTime();
+            future = current.AddHours(response);
+            Console.WriteLine("In {0} hours, it will be: {1}", response, future);
+            Console.ReadLine();
+        }
+    }
+}
