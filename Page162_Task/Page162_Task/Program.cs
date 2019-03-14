3a08f1e6e8e29197bcce227fdf6391dee703af50
Thamel90 <47161302+Thamel90@users.noreply.github.com>
2019-03-14 09:10:27 -0400

    Add Page 162 Task

---
+++ Page162_Task/Page162_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page162_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double CelsiusToKelvin = 273.15;
+            var TempCelsius = 24.5;
+        
+            var TempKelvin = TempCelsius + CelsiusToKelvin;
+            Console.WriteLine("It is {0} deg C, or {1} K", TempCelsius, TempKelvin);
+            Console.ReadLine();
+
+            Student student1 = new Student();
+            Student student2 = new Student("Trey");
+            Student student3 = new Student("Caleb", 10);
+        }
+    }
+}
---
+++ Page162_Task/Page162_Task/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page162_Task
+{
+    public class Student
+    {
+        public Student() : this("NA", 0)
+        {
+
+        }
+        public Student(string name) : this(name, 0)
+        {
+            Name = name;
+        }
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public int Grade { get; set; }
+        public string Name { get; set; }
+    }
+}
